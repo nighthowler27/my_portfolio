307ac903d54a815223c4e27ec54a6402feb0fd21
nighthowler27 <128471906+nighthowler27@users.noreply.github.com>
2023-06-20 01:14:58 +0800

    stabilize

--- components/appViewer/codeEditor.js
+++ components/appViewer/codeEditor.js
@@ -3,20 +3,27 @@ import React, { useState } from 'react';
 import styles from './codeEditor.module.css';
 import Window from '../window/window';
 
-const CodeEditor = ()  => {
+const CodeEditor = () => {
   const [htmlCode, setHtmlCode] = useState('');
   const [cssCode, setCssCode] = useState('');
   const [jsCode, setJsCode] = useState('');
   const [fontSize, setFontSize] = useState(13);
 
-  function updatePreview() {
-    // No need to manipulate the DOM directly
-    // Instead, set the state variables to trigger a re-render
-  }
+  const handleHtmlCodeChange = (event) => {
+    setHtmlCode(event.target.value);
+  };
 
-  function changeFontSize(event) {
+  const handleCssCodeChange = (event) => {
+    setCssCode(event.target.value);
+  };
+
+  const handleJsCodeChange = (event) => {
+    setJsCode(event.target.value);
+  };
+
+  const handleFontSizeChange = (event) => {
     setFontSize(event.target.value);
-  }
+  };
 
   const previewStyle = {
     fontSize: `${fontSize}px`,
@@ -24,47 +31,83 @@ const CodeEditor = ()  => {
 
   return (
     <div className={styles.container}>
-
-        <div className={styles.codeEditor}>
-            <div>
-                <h1 className={styles.header}>Code Editor</h1>
-            </div>
-
-            <div className={styles.codeBox}>
-                <div className={styles.codeBoxHtml}>
-                    <h2 className={styles.htmlHeader}>HTML</h2>
-                    <textarea
-                    value={htmlCode}
-                    onChange={(e) => setHtmlCode(e.target.value)}
-                    rows="10"
-                    className={styles.htmlTxt}
-                    ></textarea>
-                </div>
-
-                <div className={styles.codeBoxCss}>
-                    <h2 className={styles.cssHeader}>CSS</h2>
-                    <textarea
-                    value={cssCode}
-                    onChange={(e) => setCssCode(e.target.value)}
-                    rows="10"
-                    className={styles.cssTxt}
-                    ></textarea>
-                </div>
-
-                <div className={styles.codeBoxJs}>
-                    <h2 className={styles.jsHeader}>JavaScript</h2>
-                    <textarea
-                    value={jsCode}
-                    onChange={(e) => setJsCode(e.target.value)}
-                    rows="10"
-                    className={styles.jsTxt}
-                    ></textarea>
-                </div>
-            </div>
+      <div className={styles.codeEditor}>
+        <div>
+          <h1 className={styles.header}>Code Editor</h1>
         </div>
 
-      <div className={styles.editorViewer}>
+        <div className={styles.codeBox}>
+          <div className={styles.HTMLcontainer}>
+            <h2 className={styles.htmlHeader}>HTML</h2>
+            <div className={styles.codeBoxHtml}>
+              <div className={styles.editBoxBG}>
+                <div className={styles.textBoxEditor}>
+                  <div className={styles.lineNumbers}>
+                    {htmlCode.split('\n').map((_, index) => (
+                      <div key={index}>{index + 1}</div>
+                    ))}
+                  </div>
+                  <textarea
+                    value={htmlCode}
+                    onChange={handleHtmlCodeChange}
+                    rows="10"
+                    className={`${styles.htmlTxt} ${styles.noResize}`}
+                    onScroll={(e) => {
+                      const textarea = e.target;
+                      const lineNumbers = textarea.previousElementSibling;
+                      lineNumbers.scrollTop = textarea.scrollTop;
+                    }}
+                  ></textarea>
+                </div>
+              </div>
+            </div>
+          </div>
 
+          <div className={styles.CSScontainer}>
+            <h2 className={styles.cssHeader}>CSS</h2>
+            <div className={styles.codeBoxHtml}>
+              <div className={styles.editBoxBG}>
+                <div className={styles.textBoxEditor}>
+                  <div className={styles.lineNumbers}>
+                    {cssCode.split('\n').map((_, index) => (
+                      <div key={index}>{index + 1}</div>
+                    ))}
+                  </div>
+                  <textarea
+                    value={cssCode}
+                    onChange={handleCssCodeChange}
+                    rows="10"
+                    className={`${styles.cssTxt} ${styles.noResize}`}
+                  ></textarea>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className={styles.JScontainer}>
+            <h2 className={styles.jsHeader}>JavaScript</h2>
+            <div className={styles.codeBoxHtml}>
+              <div className={styles.editBoxBG}>
+                <div className={styles.textBoxEditor}>
+                  <div className={styles.lineNumbers}>
+                    {jsCode.split('\n').map((_, index) => (
+                      <div key={index}>{index + 1}</div>
+                    ))}
+                  </div>
+                  <textarea
+                    value={jsCode}
+                    onChange={handleJsCodeChange}
+                    rows="10"
+                    className={`${styles.jsTxt} ${styles.noResize}`}
+                  ></textarea>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className={styles.editorViewer}>
         <div className={styles.previewContainer}>
           <div className={styles.browserFrame}>
             <div className={styles.browserContent}>
@@ -77,15 +120,10 @@ const CodeEditor = ()  => {
           </div>
         </div>
 
-        <Window className={styles.browserBG}/>
-        
+        <Window className={styles.browserBG} />
       </div>
-{/* 
-      <button onClick={updatePreview} className={styles.prevBtn}>
-        Update Preview
-      </button> */}
     </div>
   );
-}
+};
 
 export default CodeEditor;
