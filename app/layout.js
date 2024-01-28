eff20204f5e28a3b1016f2f8f91a746a85598af1
nighthowler27 <128471906+nighthowler27@users.noreply.github.com>
2024-01-28 20:58:06 +0800

    update landing page

--- app/layout.js
+++ app/layout.js
@@ -5,7 +5,6 @@ import Navbar from '@/components/navbar/Navbar';
 import Footer from '@/components/footer/Footer';
 import { ThemeContextProvider } from "@/context/ThemeContext";
 import ThemeProvider from "@/providers/ThemeProvider";
-import AuthProvider from "@/providers/AuthProvider";
 
 
 const inter = Inter({ subsets: ['latin'] })
@@ -20,7 +19,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <AuthProvider>
+        
             <ThemeContextProvider>
                 <ThemeProvider>
                     <div className="container">
@@ -34,7 +33,7 @@ export default function RootLayout({ children }) {
                     </div>
                 </ThemeProvider>
             </ThemeContextProvider>
-        </AuthProvider>
+        
       </body>
     </html>
   )
