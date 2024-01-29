d5c3094f20a4fea68131903963a2e5804b85f8ba
nighthowler27 <128471906+nighthowler27@users.noreply.github.com>
2024-01-30 03:41:07 +0800

    update layout and portfolio nav

--- components/adStrip/Adstrip.js
+++ components/adStrip/Adstrip.js
@@ -10,6 +10,10 @@ const Adstrip = () => {
             <p>+63 (02)8584-3542</p>
         </div>
         <div className={styles.message}>
+            <p>Looking for professional Photo Editor???</p>
+            <p>How about an amazing Web App Developer???</p>
+        </div>
+        <div className={styles.message}>
             <p>Avail the services that I offer!!!</p>
             <p>+63 917-476-5836</p>
         </div>
